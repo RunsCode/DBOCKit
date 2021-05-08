01a85b16308a66e322d3f0ffa9480d99f1e576e5
WangYajun <wangyajun05822@hellobike.com>
2021-05-08 19:49:50 +0800

    feat(add db file):

---
+++ DBOCKit/Classes/DBFile.h
@@ -0,0 +1,18 @@
+//
+//  DBFile.h
+//  DBOCKit
+//
+//  Created by WangYajun on 2021/5/8.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DBFile : NSObject
+
++ (NSString *)pathWithName:(NSString *)fName directory:(NSString *)dir;
+
+@end
+
+NS_ASSUME_NONNULL_END
